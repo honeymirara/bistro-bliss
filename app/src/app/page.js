a6b1dbede077180495cc4101d666021c791ccdb8
honeymirara <honeymirara@gmail.com>
2023-11-25 09:27:32 -1000

    header

--- app/src/app/page.js
+++ app/src/app/page.js
@@ -1,6 +1,7 @@
 import Header from '@/components/Header/Header'
 import styles from './page.module.css'
 
+
 export default function Home() {
   return (
     <>
--- app/src/components/Header/Header.js
+++ app/src/components/Header/Header.js
@@ -1,3 +1,4 @@
+import Icons from './Icons';
 import style from './style.module.scss';
 
 export default function Header() {
@@ -5,27 +6,39 @@ export default function Header() {
         <>
             <div className={style.contacts}>
                 <div className={style.info}>
-                    <div className={style.phone}></div>
-                    <p>(414) 857 - 0107</p>
-                    <div className={style.postcard}></div>
-                    <p>yummy@bistrobliss</p>
+                    <div className={style.firstBlock}>
+                        <div className={style.phone}></div>
+                        <p>(414) 857 - 0107</p>
+                    </div>
+                    <div className={style.secondBlock}>
+                        <div className={style.postcard}></div>
+                        <p>yummy@bistrobliss</p>
+                    </div>
+
                 </div>
                 <div className={style.media}>
-                    <div className={style.first}></div>
-                    <div className={style.second}></div>
-                    <div className={style.third}></div>
-                    <div className={style.fourth}></div>
+                    <Icons id='bird' />
+                    <Icons id='facebook' />
+                    <Icons id='insta' />
+                    <Icons id='github' />
                 </div>
             </div>
 
-            <div className={style.navigation}>
-                <div className={style.logo}>
-                    <div className={style.logoImg}></div>
-                    <h1>Bistro Bliss</h1>
+            <div>
+                <div className={style.navigation}>
+                    <div className={style.logo}>
+                        <div className={style.logoImg}></div>
+                        <h1>Bistro Bliss</h1>
+                    </div>
+                    <nav className={style.nav}>
+                        {/* <Link href={'/'}> <p className={pathname === '/' ? style.active : null}>Home</p> </Link>
+                        <Link href={'/about'}> <p className={pathname === '/about' ? style.active : null}>About</p> </Link>
+                        <Link href={'/menu'}> <p className={pathname === '/menu' ? style.active : null}>Menu</p> </Link> */}
+                    </nav>
+                    <div className={style.buttonBook}></div>
                 </div>
-                <nav></nav>
-                <div className={style.buttonBook}></div>
             </div>
+
         </>
     )
 }
