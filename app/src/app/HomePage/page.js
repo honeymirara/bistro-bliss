71e5002093293f1bd0c6e1eabe4d7620983aaa75
honeymirara <honeymirara@gmail.com>
2023-11-29 12:55:08 -1000

    menu-section

--- app/src/app/HomePage/page.js
+++ app/src/app/HomePage/page.js
@@ -6,7 +6,7 @@ import Footer from '@/components/Footer/Footer'
 export default function HomePage() {
     return (
         <>
-           
+
             <section className={style.preview}>
                 <div className={style.wrapper}>
                     <h1>Best food for your taste</h1>
@@ -20,30 +20,33 @@ export default function HomePage() {
 
             <section className={style.menu}>
                 <h2>Browse Our Menu</h2>
-                <div className={style.breakfast}>
-                    <div className={style.cupImg}></div>
-                    <h3>Breakfast</h3>
-                    <p>In the new era of technology we look in the future with certainty and pride for our life.</p>
-                    <h4>Explore menu</h4>
+                <div className={style.meals}>
+                    <div className={style.breakfast}>
+                        <div className={style.cupImg}></div>
+                        <h3>Breakfast</h3>
+                        <p>In the new era of technology we look in the future with certainty and pride for our life.</p>
+                        <h4>Explore menu</h4>
+                    </div>
+                    <div className={style.mainDishes}>
+                        <div className={style.dishImg}></div>
+                        <h3>Main Dishes</h3>
+                        <p>In the new era of technology we look in the future with certainty and pride for our life.</p>
+                        <h4>Explore menu</h4>
+                    </div>
+                    <div className={style.drinks}>
+                        <div className={style.drinkImg}></div>
+                        <h3>Drinks</h3>
+                        <p>In the new era of technology we look in the future with certainty and pride for our life.</p>
+                        <h4>Explore menu</h4>
+                    </div>
+                    <div className={style.desserts}>
+                        <div className={style.cakeImg}></div>
+                        <h3>Desserts</h3>
+                        <p>In the new era of technology we look in the future with certainty and pride for our life.</p>
+                        <h4>Explore menu</h4>
+                    </div>
                 </div>
-                <div className={style.mainDishes}>
-                    <div className={style.dishImg}></div>
-                    <h3>Main Dishes</h3>
-                    <p>In the new era of technology we look in the future with certainty and pride for our life.</p>
-                    <h4>Explore menu</h4>
-                </div>
-                <div className={style.drinks}>
-                    <div className={style.drinkImg}></div>
-                    <h3>Drinks</h3>
-                    <p>In the new era of technology we look in the future with certainty and pride for our life.</p>
-                    <h4>Explore menu</h4>
-                </div>
-                <div className={style.desserts}>
-                    <div className={style.cakeImg}></div>
-                    <h3>Desserts</h3>
-                    <p>In the new era of technology we look in the future with certainty and pride for our life.</p>
-                    <h4>Explore menu</h4>
-                </div>
+
             </section>
 
             <section className={style.events}>
@@ -70,7 +73,7 @@ export default function HomePage() {
                     </div>
                 </div>
             </section>
-           
+
         </>
     )
 }
