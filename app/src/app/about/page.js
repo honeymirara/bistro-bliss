a68213f47b8ca3d86f5adf96dcac422ee3601357
honeymirara <honeymirara@gmail.com>
2023-12-05 20:34:33 +0300

    section1-about

--- app/src/app/about/page.js
+++ app/src/app/about/page.js
@@ -1,14 +1,14 @@
 import style from './style.module.scss';
 
-export default function AboutPage() {
+export default function about() {
     return (
         <>
-            <section className={style.healtyFood}>
+            <section className={style.healthyFood}>
                 <div className={style.wrapper}>
-                    <div className={style.pastaImg}></div>
+                    <div className={style.imagePasta}></div>
                     <div className={style.information}>
                         <h1>We provide healthy food for your family.</h1>
-                        <p1>Our story began with a vision to create a unique dining experience that merges fine dining, exceptional service, and a vibrant ambiance. Rooted in city's rich culinary culture, we aim to honor our local roots while infusing a global palate.</p1>
+                        <p className={style.firstParagraph}>Our story began with a vision to create a unique dining experience that merges fine dining, exceptional service, and a vibrant ambiance. Rooted in city's rich culinary culture, we aim to honor our local roots while infusing a global palate.</p>
                         <p2>At place, we believe that dining is not just about food, but also about the overall experience. Our staff, renowned for their warmth and dedication, strives to make every visit an unforgettable event.</p2>
                     </div>
                 </div>
